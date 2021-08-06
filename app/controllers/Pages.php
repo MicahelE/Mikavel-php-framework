ec6935203748b9d93482bd4e6ad283a98922f4c7
MicahelE <imohgenius@yahoo.com>
2021-08-06 13:54:53 +0100

    added call back function

---
+++ app/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+class Pages {
+public function __construct(){
+   
+}
+
+public function index()
+{
+    
+}
+
+public function about($id)
+{
+     echo $id;
+}
+}
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -13,9 +13,34 @@ Class Core {
     {
         $url =  $this->getUrl();
         // Look in controllers for first value
-        if (file_exists('../app/controllers/' . ucwords($) )) {
-            # code...
+        if (file_exists('../app/controllers/' . ucwords($url[0]). '.php' )) {
+            // var_dump($url[1]);
+            # If exists, set as controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 index  
+            unset($url[0]);
+           
         }
+        //  var_dump($url[0]);
+        //Require the controller
+        require_once '../app/controllers/' .  $this->currentController . '.php';
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
+
+        // Checksfor second part of url
+        if (isset($url[1])) {
+            // Check to see if method exists in controller
+            if (method_exists($this->currentController, $url[1])) {
+               $this->currentMethod= $url[1];
+               // Unset 1 index  
+            unset($url[1]);
+              
+            }
+        }
+        // echo $this->currentMethod;
+        $this->params = $url ? array_values($url) : [];
+        //Call a callback with array of params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl()
@@ -25,6 +50,7 @@ Class Core {
        $url= filter_var($url, FILTER_SANITIZE_URL);
        $url = explode('/', $url);
        return $url;
+    //    var_dump($url[1]);
     }
     }
 }
